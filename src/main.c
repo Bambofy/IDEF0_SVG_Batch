7e67a982c296efbb1546148021038e849271b269
Richard Bamford <rbamfordz@gmail.com>
2021-11-15 11:28:28 +0000

    each generated file is prepended with its node number.

--- src/main.c
+++ src/main.c
@@ -12,14 +12,25 @@ int main(int argc, char **argv) {
     dir = opendir(argv[1U]);
 
     while ((dirent = readdir(dir)) != NULL) {
-        FILE* toc_process;
-        char file_path[256U];
-        char toc_command[256U];
-        char toc_result_buffer[512U];
-
         if (strstr(dirent->d_name, ".idef") == NULL) {
             continue;
         } else {
+            FILE* toc_process;
+            char file_path[256U];
+            char toc_command[256U];
+            char toc_result_buffer[512U];
+            unsigned int activity_number_stack[512U];
+            size_t activity_number_stack_top;
+            unsigned int current_activity_depth;
+            unsigned int previous_activity_depth;
+            unsigned int activity_number_counter;
+
+            current_activity_depth = 0;
+            previous_activity_depth = 0;
+            activity_number_counter = 0;
+
+            activity_number_stack_top = 0U;
+
             strcpy(file_path, argv[1U]);
             strcat(file_path, "/");
             strcat(file_path, dirent->d_name);
@@ -34,6 +45,7 @@ int main(int argc, char **argv) {
                 FILE* decompose_process;
                 char decompose_command[2048U];
                 char activity_name[512U];
+                char activity_number[512U];
 
                 for (unsigned int i = 0U; i < 512U; i++) {  // snip the spaces from the front of the name.
                     if (isspace(toc_result_buffer[i]) == 0) {
@@ -48,6 +60,43 @@ int main(int argc, char **argv) {
                     }
                 }
 
+                for (unsigned int i = 0U; i < 512U; i++) {  // calculate the node number from the depth.
+                    if (isspace(toc_result_buffer[i]) == 0) {
+                        previous_activity_depth = current_activity_depth;
+                        current_activity_depth = i / 2U;
+
+                        if (current_activity_depth > previous_activity_depth) {
+                            activity_number_counter = 0U;
+                            activity_number_stack_top++;
+                            activity_number_stack[activity_number_stack_top] = activity_number_counter;
+                        } else if (current_activity_depth < previous_activity_depth) {
+                            activity_number_stack_top--;
+                            activity_number_counter = activity_number_stack[activity_number_stack_top];
+                            activity_number_counter++;
+                            activity_number_stack[activity_number_stack_top] = activity_number_counter;
+                        } else {
+                            activity_number_counter++;
+                            activity_number_stack[activity_number_stack_top] = activity_number_counter;
+                        }
+
+                        break;
+                    }
+                }
+
+                memset(activity_number, 0, 512U);
+
+                for (unsigned int i = 0U; i < activity_number_stack_top + 1U; i++) {
+                    char activity_number_text[32U];
+
+                    sprintf(activity_number_text, "%d", activity_number_stack[i]);
+
+                    if (i == 0U) {
+                        strcpy(activity_number, activity_number_text);
+                    } else {
+                        strcat(activity_number, activity_number_text);
+                    }
+                }
+
                 memset(toc_result_buffer, 0, 512U);
 
                 strcpy(decompose_command, "decompose");
@@ -59,7 +108,9 @@ int main(int argc, char **argv) {
                 strcat(decompose_command, "\"");
                 strcat(decompose_command, " >\"");
                 strcat(decompose_command, argv[1U]);
-                strcat(decompose_command, "/");
+                strcat(decompose_command, "/A");
+                strcat(decompose_command, activity_number);
+                strcat(decompose_command, "_");
                 strcat(decompose_command, activity_name);
                 strcat(decompose_command, "\".svg");
 
